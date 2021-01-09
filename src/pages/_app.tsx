286294399fafb374d50cd12c1278d5c224545ce2
mateuspntx <mateuspntx@gmail.com>
2021-01-08 23:00:19 -0300

    chore: getting started

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import { AppProps } from 'next/app';
+
+import GlobalStyle from '../styles/GlobalStyles';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <>
+    <Component {...pageProps} />
+    <GlobalStyle />
+  </>
+);
 
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -43,13 +43,10 @@ export default class MyDocument extends Document {
       <Html lang="pt">
         <Head>
           <meta charSet="utf-8" />
-
           <link
             href="https://fonts.googleapis.com/css?family=Roboto:400,500,700"
             rel="stylesheet"
           />
-
-          <link rel="icon" href="https://rocketseat.com.br/favicon.ico" />
         </Head>
         <body>
           <Main />
