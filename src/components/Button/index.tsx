b90e0672e98c9a2198abd8f38582f303cb57a9ce
mateuspntx <mateuspntx@gmail.com>
2021-01-11 02:30:10 -0300

    chore: add button type

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,16 +4,25 @@ import { ButtonContainer } from './style';
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   href?: string;
   className?: string;
+  type?: 'submit' | 'reset' | 'button';
   disabled?: boolean;
   variant?: 'border-only' | 'gray' | 'no-color';
   rounded?: boolean;
 }
 
 const Button: React.FC<ButtonProps> = (props) => {
-  const { variant, disabled, rounded, children, ...rest } = props;
+  const {
+    type = 'button',
+    variant,
+    disabled,
+    rounded,
+    children,
+    ...rest
+  } = props;
 
   return (
     <ButtonContainer
+      type={type}
       variant={variant}
       disabled={disabled}
       rounded={rounded}
