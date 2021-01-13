b47475536676d626871e0b6744eec7283e46ede7
mateuspntx <mateuspntx@gmail.com>
2021-01-12 23:43:39 -0300

    chore: implemented ProductCard and get data via API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,53 @@
+import { useState } from 'react';
+import { getAllProducts } from '../services/api';
+
 import Topbar from '../components/Topbar';
 import PageContainer from '../components/PageContainer';
 import Header from '../components/Header';
+import ProductCard from '../components/ProductCard';
+import Row from '../components/Row';
 
-const Home: React.FC = () => {
+interface ProductsListData {
+  id: string;
+  category: string;
+  name: string;
+  price: number;
+  image_uri: string;
+}
+interface HomeProps {
+  productsListData: ProductsListData[];
+}
+
+const Home: React.FC<HomeProps> = ({ productsListData }) => {
   return (
     <>
       <Topbar />
       <PageContainer>
         <Header />
+        <Row>
+          {productsListData.map((product) => (
+            <ProductCard
+              key={product.id}
+              category={product.category}
+              name={product.name}
+              price={product.price}
+              imageSrc={product.image_uri}
+            />
+          ))}
+        </Row>
       </PageContainer>
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  const productsListData = (await getAllProducts()) || [];
+
+  return {
+    props: {
+      productsListData,
+    },
+  };
 };
 
 export default Home;
