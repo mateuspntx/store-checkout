6185e1f7e96149ec5368b4d4a35ad74fbaa15181
mateuspntx <mateuspntx@gmail.com>
2021-01-18 02:43:36 -0300

    chore: add Link to logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { FavoriteIcon, ShoppingBagIcon } from '../../utils/Icons';
 import { Container, LogoContainer, ActionsContainer } from './style';
 
@@ -8,14 +9,28 @@ const Header: React.FC = () => {
   return (
     <Container>
       <LogoContainer>
-        <h1>Storrr</h1>
+        <Link href="/">
+          <h1>Storrr</h1>
+        </Link>
       </LogoContainer>
       <ActionsContainer>
         <Button variant="no-color">
-          <Image src={FavoriteIcon} alt="Favorites" width="32" height="32" />
+          <Image
+            src={FavoriteIcon}
+            alt="Favorites"
+            width="32"
+            height="32"
+            priority
+          />
         </Button>
         <Button variant="no-color">
-          <Image src={ShoppingBagIcon} alt="Cart" width="32" height="32" />
+          <Image
+            src={ShoppingBagIcon}
+            alt="Cart"
+            width="32"
+            height="32"
+            priority
+          />
         </Button>
       </ActionsContainer>
     </Container>
