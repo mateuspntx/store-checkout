2239538a2a226ca8f63fb99d6561f6a059165710
mateuspntx <mateuspntx@gmail.com>
2021-02-06 00:09:57 -0300

    chore: setup redux persist to store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,20 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import GlobalStyle from '../styles/GlobalStyles';
 import theme from '../styles/theme';
-import { store } from '../store';
+import { store, persistor } from '../store';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </ThemeProvider>
+    <PersistGate persistor={persistor}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </ThemeProvider>
+    </PersistGate>
   </Provider>
 );
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,30 @@
 import { Middleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { persistReducer } from 'redux-persist';
+import { persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import createStore from './createStore';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
+const persistConfig = {
+  key: 'storrr',
+  storage,
+  whitelist: ['user'],
+};
+
 const sagaMiddleware = createSagaMiddleware();
 
 const middlewares: Middleware[] = [sagaMiddleware];
 
-const store = createStore(rootReducer, middlewares);
+const store = createStore(
+  persistReducer(persistConfig, rootReducer),
+  middlewares
+);
+
+const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
-export { store };
+export { store, persistor };
